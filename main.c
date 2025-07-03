c72a6c4fa4413e4c243acfb8ef9c2036e91dd897
usafdev <naffy935@gmail.com>
2025-07-03 19:56:49 +0100

    Added remove from queue button and ability to view queue

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+// Full working MP3 player with fixed next and remove behavior
 #include <stdio.h>
 #include <windows.h>
 #include <commdlg.h>
@@ -6,17 +7,24 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define ID_BUTTON_OPEN 1
-#define ID_BUTTON_PAUSE 2
+#define ID_BUTTON_OPEN   1
+#define ID_BUTTON_PAUSE  2
+#define ID_BUTTON_NEXT   3
+#define ID_BUTTON_REMOVE 4
+#define ID_LISTBOX_QUEUE 5
 #define FRAMES_PER_BUFFER 4096
 
-HWND hwndMain, hwndPauseBtn;
+HWND hwndMain, hwndPauseBtn, hwndNextBtn, hwndRemoveBtn, hwndListBox;
 HANDLE playThread = NULL;
 PaStream *stream = NULL;
 
 volatile int isPlaying = 0;
 volatile int isPaused = 0;
 volatile int stopPlayback = 0;
+volatile int skipToNext = 0;
+volatile int nextPressed = 0; // Added: distinguish next button from remove
+
+size_t currentTrackIndex = 0;
 
 typedef struct {
     char **files;
@@ -40,7 +48,31 @@ void AddToPlaylist(const char *filename) {
         playlist.files = newFiles;
         playlist.capacity = newCap;
     }
-    playlist.files[playlist.count++] = _strdup(filename);
+    playlist.files[playlist.count] = _strdup(filename);
+    SendMessage(hwndListBox, LB_ADDSTRING, 0, (LPARAM)filename);
+    playlist.count++;
+    LeaveCriticalSection(&playlistLock);
+}
+
+void RemoveFromPlaylist(size_t index) {
+    EnterCriticalSection(&playlistLock);
+    if (index >= playlist.count) {
+        LeaveCriticalSection(&playlistLock);
+        return;
+    }
+    free(playlist.files[index]);
+    for (size_t i = index; i < playlist.count - 1; ++i) {
+        playlist.files[i] = playlist.files[i + 1];
+    }
+    playlist.count--;
+    SendMessage(hwndListBox, LB_DELETESTRING, index, 0);
+
+    if (index == currentTrackIndex) {
+        skipToNext = 1;
+        nextPressed = 0;  // Remove: do NOT advance index here, playlist shifted left
+    } else if (index < currentTrackIndex && currentTrackIndex > 0) {
+        currentTrackIndex--;
+    }
     LeaveCriticalSection(&playlistLock);
 }
 
@@ -61,28 +93,14 @@ DWORD WINAPI PlayMP3Queue(LPVOID lpParam) {
 
     mpg123_init();
     mpg123_handle *mh = mpg123_new(NULL, NULL);
-    if (!mh) {
-        MessageBox(hwndMain, "Failed to init mpg123", "Error", MB_OK | MB_ICONERROR);
-        return 0;
-    }
+    if (!mh) return 0;
     mpg123_format_all(mh);
 
     PaError err = Pa_Initialize();
-    if (err != paNoError) {
-        MessageBox(hwndMain, "Failed to initialize PortAudio", "Error", MB_OK | MB_ICONERROR);
-        mpg123_delete(mh);
-        mpg123_exit();
-        return 0;
-    }
+    if (err != paNoError) return 0;
 
-    unsigned char *buffer = malloc(FRAMES_PER_BUFFER * 2 * sizeof(short) * 2); // max buffer, stereo 16bit
-    if (!buffer) {
-        MessageBox(hwndMain, "Failed to allocate buffer", "Error", MB_OK | MB_ICONERROR);
-        Pa_Terminate();
-        mpg123_delete(mh);
-        mpg123_exit();
-        return 0;
-    }
+    unsigned char *buffer = malloc(FRAMES_PER_BUFFER * 2 * sizeof(short) * 2);
+    if (!buffer) return 0;
 
     isPlaying = 1;
     isPaused = 0;
@@ -92,87 +110,82 @@ DWORD WINAPI PlayMP3Queue(LPVOID lpParam) {
         EnterCriticalSection(&playlistLock);
         if (playlist.count == 0) {
             LeaveCriticalSection(&playlistLock);
-            Sleep(100); // no files, wait
+            Sleep(100);
             continue;
         }
-        // Make a copy of current playlist count and files pointer to avoid holding lock during play
-        size_t count = playlist.count;
-        char **files = playlist.files;
+        if (currentTrackIndex >= playlist.count) currentTrackIndex = 0;
+        char *file = _strdup(playlist.files[currentTrackIndex]);
         LeaveCriticalSection(&playlistLock);
 
-        for (size_t track = 0; track < count; track++) {
-            if (stopPlayback) break;
+        if (mpg123_open(mh, file) != MPG123_OK) {
+            free(file);
+            EnterCriticalSection(&playlistLock);
+            currentTrackIndex = (currentTrackIndex + 1) % playlist.count;
+            LeaveCriticalSection(&playlistLock);
+            continue;
+        }
 
-            // Open current track
-            if (mpg123_open(mh, files[track]) != MPG123_OK) {
-                char errMsg[512];
-                snprintf(errMsg, sizeof(errMsg), "Failed to open MP3:\n%s", files[track]);
-                MessageBox(hwndMain, errMsg, "Error", MB_OK | MB_ICONERROR);
-                continue; // skip to next
-            }
+        long rate;
+        int channels, encoding;
+        if (mpg123_getformat(mh, &rate, &channels, &encoding) != MPG123_OK || encoding != MPG123_ENC_SIGNED_16) {
+            mpg123_close(mh);
+            free(file);
+            EnterCriticalSection(&playlistLock);
+            currentTrackIndex = (currentTrackIndex + 1) % playlist.count;
+            LeaveCriticalSection(&playlistLock);
+            continue;
+        }
 
-            long rate;
-            int channels, encoding;
-            if (mpg123_getformat(mh, &rate, &channels, &encoding) != MPG123_OK || encoding != MPG123_ENC_SIGNED_16) {
-                MessageBox(hwndMain, "Unsupported MP3 format", "Error", MB_OK | MB_ICONERROR);
-                mpg123_close(mh);
-                continue;
-            }
-
-            if (stream) {
-                Pa_StopStream(stream);
-                Pa_CloseStream(stream);
-                stream = NULL;
-            }
-            err = Pa_OpenDefaultStream(&stream, 0, channels, paInt16, rate,
-                                       FRAMES_PER_BUFFER, NULL, NULL);
-            if (err != paNoError) {
-                MessageBox(hwndMain, "Failed to open audio stream", "Error", MB_OK | MB_ICONERROR);
-                mpg123_close(mh);
-                continue;
-            }
-            err = Pa_StartStream(stream);
-            if (err != paNoError) {
-                MessageBox(hwndMain, "Failed to start audio stream", "Error", MB_OK | MB_ICONERROR);
-                Pa_CloseStream(stream);
-                stream = NULL;
-                mpg123_close(mh);
-                continue;
-            }
-
-            char nowPlaying[512];
-            snprintf(nowPlaying, sizeof(nowPlaying), "Playing: %s", files[track]);
-            SetWindowText(hwndMain, nowPlaying);
-
-            size_t done = 0;
-            int ret;
-
-            while ((ret = mpg123_read(mh, buffer, FRAMES_PER_BUFFER * channels * sizeof(short), &done)) == MPG123_OK) {
-                if (stopPlayback) break;
-
-                while (isPaused) {
-                    Sleep(100);
-                    if (stopPlayback) break;
-                }
-                if (stopPlayback) break;
-
-                if (done > 0) {
-                    size_t samples = done / (sizeof(short) * channels);
-                    err = Pa_WriteStream(stream, buffer, samples);
-                    if (err != paNoError) {
-                        MessageBox(hwndMain, "PortAudio write error", "Error", MB_OK | MB_ICONERROR);
-                        break;
-                    }
-                }
-            }
-
+        if (stream) {
             Pa_StopStream(stream);
             Pa_CloseStream(stream);
             stream = NULL;
-            mpg123_close(mh);
-            if (stopPlayback) break;
         }
-        // Loop again forever
+        err = Pa_OpenDefaultStream(&stream, 0, channels, paInt16, rate,
+                                   FRAMES_PER_BUFFER, NULL, NULL);
+        if (err != paNoError) {
+            mpg123_close(mh);
+            free(file);
+            EnterCriticalSection(&playlistLock);
+            currentTrackIndex = (currentTrackIndex + 1) % playlist.count;
+            LeaveCriticalSection(&playlistLock);
+            continue;
+        }
+        Pa_StartStream(stream);
+
+        char nowPlaying[512];
+        snprintf(nowPlaying, sizeof(nowPlaying), "Playing: %s", file);
+        SetWindowText(hwndMain, nowPlaying);
+
+        size_t done;
+        int ret;
+        skipToNext = 0;
+
+        while (!skipToNext && !stopPlayback && (ret = mpg123_read(mh, buffer, FRAMES_PER_BUFFER * channels * sizeof(short), &done)) == MPG123_OK) {
+            while (isPaused && !stopPlayback) Sleep(100);
+            if (done > 0 && !stopPlayback) {
+                size_t samples = done / (sizeof(short) * channels);
+                err = Pa_WriteStream(stream, buffer, samples);
+                if (err != paNoError) break;
+            }
+        }
+
+        mpg123_close(mh);
+        free(file);
+
+        EnterCriticalSection(&playlistLock);
+        if (playlist.count > 0) {
+            if (nextPressed) {
+                currentTrackIndex = (currentTrackIndex + 1) % playlist.count;
+                nextPressed = 0;
+            } else if (!skipToNext) {
+                currentTrackIndex = (currentTrackIndex + 1) % playlist.count;
+            }
+            // if skipToNext and nextPressed == 0 (remove case), do NOT advance index here
+        } else {
+            currentTrackIndex = 0; // reset if playlist empty
+        }
+        LeaveCriticalSection(&playlistLock);
     }
 
     free(buffer);
@@ -185,7 +198,6 @@ DWORD WINAPI PlayMP3Queue(LPVOID lpParam) {
     stopPlayback = 0;
 
     SetWindowText(hwndMain, "MP3 Player");
-
     return 0;
 }
 
@@ -199,17 +211,14 @@ void OpenFileDialogAndAddFiles(HWND hwnd) {
     ofn.Flags = OFN_ALLOWMULTISELECT | OFN_EXPLORER | OFN_FILEMUSTEXIST;
 
     if (GetOpenFileName(&ofn)) {
-        // Multiple files may be selected, parse them
         char *ptr = filesBuffer;
         char directory[MAX_PATH];
         strcpy(directory, ptr);
         ptr += strlen(ptr) + 1;
 
         if (*ptr == 0) {
-            // Only one file selected, add it
             AddToPlaylist(directory);
         } else {
-            // Multiple files selected
             while (*ptr) {
                 char fullpath[MAX_PATH];
                 snprintf(fullpath, sizeof(fullpath), "%s\\%s", directory, ptr);
@@ -220,6 +229,7 @@ void OpenFileDialogAndAddFiles(HWND hwnd) {
 
         if (!isPlaying) {
             stopPlayback = 0;
+            currentTrackIndex = 0;
             playThread = CreateThread(NULL, 0, PlayMP3Queue, NULL, 0, NULL);
             EnableWindow(hwndPauseBtn, TRUE);
             SetWindowText(hwndPauseBtn, "Pause");
@@ -229,42 +239,48 @@ void OpenFileDialogAndAddFiles(HWND hwnd) {
 
 void TogglePause() {
     if (!isPlaying) return;
-
     isPaused = !isPaused;
-    if (isPaused) {
-        SetWindowText(hwndPauseBtn, "Resume");
-    } else {
-        SetWindowText(hwndPauseBtn, "Pause");
+    SetWindowText(hwndPauseBtn, isPaused ? "Resume" : "Pause");
+}
+
+void SkipToNext() {
+    if (isPlaying) {
+        skipToNext = 1;
+        nextPressed = 1;
     }
 }
 
+void RemoveSelectedFromQueue() {
+    int sel = SendMessage(hwndListBox, LB_GETCURSEL, 0, 0);
+    if (sel != LB_ERR) RemoveFromPlaylist((size_t)sel);
+}
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    switch(msg) {
-    case WM_COMMAND:
-        if (LOWORD(wParam) == ID_BUTTON_OPEN) {
-            OpenFileDialogAndAddFiles(hwnd);
-        } else if (LOWORD(wParam) == ID_BUTTON_PAUSE) {
-            TogglePause();
-        }
-        break;
-    case WM_CLOSE:
-        if (isPlaying) {
+    switch (msg) {
+        case WM_COMMAND:
+            switch (LOWORD(wParam)) {
+                case ID_BUTTON_OPEN: OpenFileDialogAndAddFiles(hwnd); break;
+                case ID_BUTTON_PAUSE: TogglePause(); break;
+                case ID_BUTTON_NEXT: SkipToNext(); break;
+                case ID_BUTTON_REMOVE: RemoveSelectedFromQueue(); break;
+            }
+            break;
+        case WM_CLOSE:
             stopPlayback = 1;
             if (playThread) {
                 WaitForSingleObject(playThread, 3000);
                 CloseHandle(playThread);
                 playThread = NULL;
             }
-        }
-        DestroyWindow(hwnd);
-        break;
-    case WM_DESTROY:
-        FreePlaylist();
-        DeleteCriticalSection(&playlistLock);
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hwnd, msg, wParam, lParam);
+            DestroyWindow(hwnd);
+            break;
+        case WM_DESTROY:
+            FreePlaylist();
+            DeleteCriticalSection(&playlistLock);
+            PostQuitMessage(0);
+            break;
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
     }
     return 0;
 }
@@ -275,25 +291,33 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmd, int nCmdShow) 
     WNDCLASS wc = {0};
     wc.lpfnWndProc = WndProc;
     wc.hInstance = hInst;
-    wc.lpszClassName = "MinimalMP3Window";
+    wc.lpszClassName = "MP3Window";
     RegisterClass(&wc);
 
     hwndMain = CreateWindow(wc.lpszClassName, "MP3 Player",
                             WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
-                            CW_USEDEFAULT, CW_USEDEFAULT, 360, 120,
+                            CW_USEDEFAULT, CW_USEDEFAULT, 400, 300,
                             NULL, NULL, hInst, NULL);
 
-    CreateWindow("BUTTON", "Open MP3(s)", WS_VISIBLE | WS_CHILD,
-                 30, 30, 130, 40, hwndMain, (HMENU)ID_BUTTON_OPEN, hInst, NULL);
+    CreateWindow("BUTTON", "Open", WS_VISIBLE | WS_CHILD,
+                 10, 10, 80, 30, hwndMain, (HMENU)ID_BUTTON_OPEN, hInst, NULL);
 
     hwndPauseBtn = CreateWindow("BUTTON", "Pause", WS_VISIBLE | WS_CHILD | WS_DISABLED,
-                               200, 30, 100, 40, hwndMain, (HMENU)ID_BUTTON_PAUSE, hInst, NULL);
+                                100, 10, 80, 30, hwndMain, (HMENU)ID_BUTTON_PAUSE, hInst, NULL);
+
+    hwndNextBtn = CreateWindow("BUTTON", "Next", WS_VISIBLE | WS_CHILD,
+                               190, 10, 80, 30, hwndMain, (HMENU)ID_BUTTON_NEXT, hInst, NULL);
+
+    hwndRemoveBtn = CreateWindow("BUTTON", "Remove", WS_VISIBLE | WS_CHILD,
+                                 280, 10, 80, 30, hwndMain, (HMENU)ID_BUTTON_REMOVE, hInst, NULL);
+
+    hwndListBox = CreateWindow("LISTBOX", NULL, WS_VISIBLE | WS_CHILD | WS_BORDER | LBS_NOTIFY,
+                               10, 50, 360, 200, hwndMain, (HMENU)ID_LISTBOX_QUEUE, hInst, NULL);
 
     ShowWindow(hwndMain, nCmdShow);
     UpdateWindow(hwndMain);
 
     MSG msg;
     while (GetMessage(&msg, NULL, 0, 0)) DispatchMessage(&msg);
-
     return 0;
 }
